572dd45cee1e8d695cd5897681bd5391b3549058
Anthony Chang <awchang12@gmail.com>
2018-05-15 21:54:15 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -26,6 +26,7 @@ def play(my_songs)
   if my_songs.include?(user_response)
     puts "playing #{user_response}"
   elsif user_response.to_i > 0 && user_response.to_i <= my_songs.length
+  
 end
 
 def list(my_songs)
